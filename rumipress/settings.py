7b5a9a532b94eb1561c2cbfcd8e0a2eb622dd2dc
Samuel Akuffo <131817095+KwameSA@users.noreply.github.com>
2025-09-04 05:02:37 -0400

    Update settings.py

--- rumipress/settings.py
+++ rumipress/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='unsafe-default')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
